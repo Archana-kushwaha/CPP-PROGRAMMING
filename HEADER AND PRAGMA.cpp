5dad2ea95cbac422125ccf217cea04e90809b468
Archana Kushwaha <70112830+Archana-kushwaha@users.noreply.github.com>
2020-09-03 07:42:42 +0530

    Add files via upload

---
+++ HEADER AND PRAGMA.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"HEADER1.h"
+#include"HEADER2.h"
+
+
+int main() {
+	int num = 654;
+	int num1 = 4;
+		std::cout << SumOfDigits(num) << std::endl;
+		std::cout << square(num1) << std::endl;
+	}
